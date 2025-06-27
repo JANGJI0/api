ce2f517725c698b18d6abc99796f2ac489b64061
JANGJI0 <qlvm2002@gmail.com>
2025-06-27 17:25:35 +0900

    2025.06.27 country, city, address 입력, 수정, 삭제 구현

---
+++ src/main/java/com/sakila/api/repository/AddressRepository.java
@@ -0,0 +1,12 @@
+package com.sakila.api.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sakila.api.entity.AddressEntity;
+import com.sakila.api.entity.CityEntity;
+
+public interface AddressRepository extends JpaRepository<AddressEntity, Integer> {
+	Long countByCityEntity(CityEntity cityEntity);
+}
---
+++ src/main/java/com/sakila/api/repository/CityRepository.java
@@ -0,0 +1,15 @@
+package com.sakila.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sakila.api.entity.CityEntity;
+import com.sakila.api.entity.CountryEntity;
+
+public interface CityRepository extends JpaRepository<CityEntity, Integer> {
+	Long countByCountryEntity(CountryEntity countryEntity);
+	// select count(*) from city where country_id = countryEntity.getCountryId()
+	
+	// 없어도 무관
+	//List<CityEntity> findAll();
+	// AddressEntity save ~~
+}
